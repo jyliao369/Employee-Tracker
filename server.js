cc95856d02e756fc0fcf6985381a1755e3c5000b
jyliao369 <jyliao369@gmail.com>
2021-08-25 14:01:22 -0400

    created the server.js file and added a connection between the server file and the db file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Connecting to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '', // Need to insert password here
+        database: 'tracker_db'
+    },
+    console.log('Connected to the tracker_db database')
+);
+
+// The next block of codes asks whether the user would to do one of these things:
+// View All Departments?", "View All Roles?", "View All Employees?", "Add Department?", "Add Role?", "Add Employee?", "Update Employee Role?"
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
